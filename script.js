98aca3f8b6977408d4ecc83b98a0dac8aae53d86
Akash Gupta <124385823+Gupta-G-0406@users.noreply.github.com>
2026-01-18 01:34:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function stringChop(str, size) {
-  // your code here
+  if (str === null) {
+    return [];
+  }
+  const result = [];
+  let i = 0;
+
+  while (i < str.length) {
+    
+    result.push(str.substring(i, i + len));
+    i += len;
+  }
+  return result;
 }
 
 // Do not change the code below
